ddd1fa92c8a614756f70d9e7e85fcc49941035a8
Jason Wang <521wanghp@163.com>
2017-12-25 22:47:36 +0800

    change ex1

--- ex1.py
+++ ex1.py
@@ -1,3 +1,10 @@
-print "hello world"
-print "bye world"
-print "fuck world"
+# -*- coding: UTF-8 -*-
+
+num = 0
+for i in range(1,5):
+    for j in range(1,5):
+        for k in range(1,5):
+            if(i!=j and j!=k and i!=k):
+                print str(num)+':'+str(i)+str(j)+str(k)
+                num = num+1
+print num
